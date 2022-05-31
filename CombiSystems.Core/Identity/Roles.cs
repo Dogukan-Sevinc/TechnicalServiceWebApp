f28b1b42c6039e50f7a2ebe61a06f20fb2dc745f
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-05-31 11:05:51 +0300

    Controllers ok

--- CombiSystems.Core/Identity/Roles.cs
+++ CombiSystems.Core/Identity/Roles.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CombiSystems.Core.Identity
+namespace CombiSystems.Core.Identity;
+
+public static class Roles
 {
-    internal class Roles
-    {
-    }
+    public static readonly string Admin = "Admin";
+    public static readonly string User = "User";
+    public static readonly string Passive = "Passive";
+
+    public static List<string> RoleList = new List<string>()
+{
+    Admin, User, Passive
+};
 }
