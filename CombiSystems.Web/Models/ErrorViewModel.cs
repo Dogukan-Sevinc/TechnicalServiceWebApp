15d923f06caabfb30554765506036e21ab96081b
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-06-08 14:15:35 +0300

    CombiSystems.Web ok

---
+++ CombiSystems.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CombiSystems.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
